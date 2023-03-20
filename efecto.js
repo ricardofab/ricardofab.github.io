1672f1f8fb5cdeb252e8b718898c129d54b16176
ricardofab <127355133+ricardofab@users.noreply.github.com>
2023-03-20 15:30:35 -0300

    initial commit

---
+++ efecto.js
@@ -0,0 +1,6 @@
+function ingresarm(event) {
+    document.getElementById("area").style.backgroundColor="aqua";}
+
+  function salirm(event) {
+    document.getElementById("area").style.backgroundColor="rgb(225, 235, 235)" ;}
+ 
